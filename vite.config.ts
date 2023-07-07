76b7c12a22fda6f2cd0bfc8ecd1c73bf2cca58d3
tigawanna <denniskinuthiaw@gamil.com>
2023-07-07 08:23:55 +0300

    configure vite import aliases + server:host

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from 'vite-plugin-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    host: true,
+  },
 });
