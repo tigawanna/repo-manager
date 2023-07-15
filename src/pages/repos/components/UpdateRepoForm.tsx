b0da31d32c7b226441ab2fe90c6cf7f35d06e734
tigawanna <denniskinuthiaw@gamil.com>
2023-07-15 12:43:58 +0300

    addded notifaication + theme to repo update form

--- src/pages/repos/components/UpdateRepoForm.tsx
+++ src/pages/repos/components/UpdateRepoForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Checkbox, Stack } from "@mui/material";
+import { Checkbox, Stack, Typography, useTheme } from "@mui/material";
 import { useMutation } from "@tanstack/react-query";
 import { Loader } from "lucide-react";
 import { TextField, Box } from "@mui/material";
@@ -8,6 +8,9 @@ import {
   UpdateRepositoryInput,
   updateRepository,
 } from "@/state/providers/repos/mutation/updateRepo";
+import { useNotification } from "@refinedev/core";
+import { SaveButton } from "@refinedev/mui";
+
 
 interface UpdateRepoFormProps {
   input: RepositoriesNode;
@@ -22,14 +25,30 @@ export function UpdateRepoForm({ input }: UpdateRepoFormProps) {
     hasWikiEnabled: input.hasWikiEnabled,
     template: input.isTemplate,
   });
+  const {open,close}=useNotification()
+const theme = useTheme()
   const mutation = useMutation({
     mutationFn: (variables: UpdateRepositoryInput) =>
       updateRepository(variables),
     onSuccess(data) {
       console.log("succesfully updated repository", data);
+      open?.({
+        key: "update-repo-success",
+        type: "success",
+        message: "Success",
+        description: "Repository updated successfully",
+      })
+      close?.("update-repo-success")
     },
-    onError(error) {
+    onError(error:any) {
       console.log("error updating repository", error);
+      open?.({
+        key: "update-repo-error",
+        type: "error",
+        message: "Error updating",
+        description: error?.message,
+      })
+      close?.("update-repo-error")
     },
   });
   const updateRepoBooleans = [
@@ -62,15 +81,22 @@ export function UpdateRepoForm({ input }: UpdateRepoFormProps) {
   return (
     <Box
       component="form"
-      className="flex flex-col w-full p-6 rounded-lg bg-slate-900"
+      className="flex flex-col w-full p-10 rounded-lg"
       onSubmit={handleSubmit}
       sx={{
+        backgroundColor:theme.palette.background.paper,
         "& .MuiTextField-root": { m: 1, width: "100%", fontSize: "10px" },
       }}
     >
-      <h1 className="font-bold text-lg p-1 m-1 border-b">
+      <Typography
+        variant="h5"
+        sx={{
+          color: theme.palette.text.secondary,
+          fontWeight: "bold",
+        }}
+       className="font-bold text-lg p-1 m-1">
         Update {input.nameWithOwner} repository{" "}
-      </h1>
+      </Typography>
       <TextField
         id="name"
         name="name"
@@ -118,7 +144,7 @@ export function UpdateRepoForm({ input }: UpdateRepoFormProps) {
         ))}
       </Stack>
 
-      <Button
+      <SaveButton
         type="submit"
         variant="outlined"
         color="secondary"
@@ -129,7 +155,7 @@ export function UpdateRepoForm({ input }: UpdateRepoFormProps) {
         ) : (
           "Update"
         )}
-      </Button>
+      </SaveButton>
     </Box>
   );
 }
