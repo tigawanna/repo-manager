4be1e5498ae080f5459e48bd51f6a54199d1f691
tigawanna <denniskinuthiaw@gamil.com>
2023-07-15 12:52:11 +0300

    repo card updates

--- src/pages/repos/components/RepoCard.tsx
+++ src/pages/repos/components/RepoCard.tsx
@@ -7,12 +7,14 @@ import {
   Card,
   CardMedia,
   CardContent,
+  useTheme,
 } from "@mui/material";
 import { MuiModal } from "@/components/shared/MuiModal";
 import { CardMenu } from "@/components/shared/CardMenu";
 import { ItemList, RepositoriesEdge } from "@/state/providers/repos/types";
 import { Link } from "react-router-dom";
 import { Star, GitFork } from "lucide-react";
+
 
 
 interface RepoCard {
@@ -32,6 +34,8 @@ export function RepoCard({
 }: RepoCard) {
   const [repo, setRepos] = React.useState(viewer_repos.node);
   const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+
   const topics = repo.repositoryTopics.nodes;
   const stars = repo.stargazerCount;
   const forks = repo.forkCount;
@@ -44,19 +48,21 @@ export function RepoCard({
         borderRadius: 5,
         msOverflowY: "scroll",
         ":hover": {
-          boxShadow: "1px 1px 1px 1px purple",
+          boxShadow: "1px 1px 1px 1px" + theme.palette.secondary.main,
           color: "black",
         },
       }}
       className="sm:h-[350px] w-full sm:w-[45%] lg:w-[30%] flex flex-col  gap-0 "
-      variant="elevation"
-    >
+      variant="elevation">
       <div className="w-full flex flex-wrap lg:flex-row  justify-between p-3 gap-2 ">
         <div className="w-full flex justify-between items-center gap-1">
           {editing && (
             <Checkbox
-              className=" border-purple-600 mr-1"
+              className="mr-1"
               size="small"
+              sx={{
+                color: theme.palette.secondary.main,
+              }}
               checked={selected}
               onClick={() => {
                 if (selected) {
@@ -83,8 +89,7 @@ export function RepoCard({
                   color: "blue",
                 },
               }}
-              onClick={() => setOpen(true)}
-            >
+              onClick={() => setOpen(true)}>
               Edit
             </MenuItem>
 
@@ -95,18 +100,14 @@ export function RepoCard({
                   color: "red",
                 },
               }}
-              onClick={() => setOpen(true)}
-            >
+              onClick={() => setOpen(true)}>
               Delete
             </MenuItem>
           </CardMenu>
         </div>
       </div>
 
-      <Link
-        to={"/repos/show/" + repo.name}
-        className="flex flex-col h-full hover:brightness-[80%]"
-      >
+      <Link to={"/repos/show/" + repo.name} className="flex flex-col h-full hover:brightness-[80%]">
         <CardMedia
           component="img"
           height={50}
@@ -116,9 +117,7 @@ export function RepoCard({
           width={50}
         />
         <CardContent style={{ padding: 1 }} className="w-fit p-1">
-          <h4 className="text-sm  line-clamp-1 p-1 px-2 font-light">
-            {repo.description}
-          </h4>
+          <h4 className="text-sm  line-clamp-1 p-1 px-2 font-light">{repo.description}</h4>
           <div className="flex rounded-full border shadow shadow-slate-500 w-fit">
             {stars > 0 && (
               <div className="flex gap-1 text-sm items-center justify-center px-2">
@@ -137,18 +136,9 @@ export function RepoCard({
       </Link>
 
       <div className="w-full  min-h-[50px]  flex flex-wrap gap-1  border-t p-2  scrollbar-thin overflow-x-scroll">
-        {topics.length < 1 && (
-          <Chip variant="outlined" label="Add topic" size="small" />
-        )}
+        {topics.length < 1 && <Chip variant="outlined" label={repo.name} size="small" />}
         {topics.map((topic) => {
-          return (
-            <Chip
-              key={topic.id}
-              variant="outlined"
-              label={topic.topic.name}
-              size="small"
-            />
-          );
+          return <Chip key={topic.id} variant="outlined" label={topic.topic.name} size="small" />;
         })}
       </div>
 
